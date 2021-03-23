b71b65cbfa78c0f63e1347aee7a3405c60c1ff8e
Andrew Oplas <andrewmoplas@gmail.com>
2021-03-23 15:37:56 +0800

    Create new keys (#20)

--- public/js/sign-message.js
+++ public/js/sign-message.js
@@ -39,32 +39,32 @@
  *     4. Remove or comment out any other references to "setSignaturePromise"
  */
 var privateKey = "-----BEGIN PRIVATE KEY-----\n" + 
-"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDHXB4SiY+0iLgR\n" + 
-"MFapxKPOiVRBW2w7PWanwr3W1VIiZmyVcbu3SamVVqtzik3DujWEPiGafutqDcmE\n" + 
-"s4muSRnThZBTMqlGuRSe5V/bqHGOqgqNI/eBSTbaPfG5gxEN8E31C/Luqz1epH0l\n" + 
-"PsoJx7duDFmlR2g58rY3qxxmMKQ2wfXOozWyj9oMSQAXYd7YGkyD3GitZuYMDRB7\n" + 
-"YcXd+yUJtPLMBr2qrvLdHD/IOFpDsXSX/sPiEFGD/dYauO/BRybs4toLyEBw/xX9\n" + 
-"jWKzb4zxPFBnkExlH3kJLDerIY374zDaW84oET9wqZOzrjGgyfrATroQawkmTEY5\n" + 
-"pnLcCr6PAgMBAAECggEAMZ+LWL/nBmAVuSmYGM/6mjlywIX6LdC90bpWuLgGpstp\n" + 
-"8dUKjbgHP0YAjVT6wGKEWbOBd8xp2s8tvxtVhQ1rIUyYvSzaF+GP8XFU5/9IrONE\n" + 
-"lxdBz2ui/LX4il3Gi/qp3PopcSEsuHcnM3MqpT6SORFX6x0YI3x3wOmVuxYWeg5i\n" + 
-"gbJe5ApfiGT9vjQx1voL+lSqB/7zZyXNFWZV38Xj29poTvE8nUOdN0qkMtryQ++I\n" + 
-"erDzpZmPKSldlArrk01/Uxd7WtufgTn0pObFFI91/G8H08LmaSFbpTVV0lV1PaRt\n" + 
-"1w+bSfHSdXVlh37dLFw6iZU4CZtf3OsKzmW4qlVX2QKBgQD7GbUkEwbKlcy1QFm/\n" + 
-"nFK/n0FFxRORS/b2HwcRl03Sz9UUJOIKEW5coy4lnDQ6AZJm8lJEMnqvgbe2adxT\n" + 
-"Kr817EEMOm8YlkCYWLCG+ifxBDVRHB9b9nXGoFnLQqNkUIxtbYtJat/ayKjYh9c0\n" + 
-"5nw5DKPtdhlrwt4UwBKz0vKWvQKBgQDLP/TKuz6+udjtDjWVfknJjmTtUjzmRSDh\n" + 
-"ZFnmSbmlXyQlwJ7IzscNepprYB1XwCetjiy0kAnP2j+dgs+Hmlvloak4QhJLyzds\n" + 
-"UjeEXw1ZG1JJxub+tmIv+dtmPvr92lQ2qrNjBZVB1D5jnyVU90vP9DtmicmgpeBJ\n" + 
-"JhUCL16VOwKBgBEF5iTi8VPsoesryKnX1DhPeKJdQP1ZtA1tfxIvOsRu+JgcBAMA\n" + 
-"uYWoPmmoVFYWYzF9fhxHe8/IMIc6WAh/XmUudh55G60ECq8JXDZaYTjSuzabfHjE\n" + 
-"lal4l33TL7aOS2bjxUyd5T0021WKqAlbEtLfS4G+vuxyUpH/j4UHaRDVAoGARBrC\n" + 
-"I8xqUe5kScqhGd6fSvJzzzHnRiGTyvKdTRUXJ9qheiT5D8B54oJai/BA5M52fSlO\n" + 
-"wZIiN4uiFV1nvMB0WpW45ij9iHgnshw6jhhrmJgXErGbZW4nqHvIkJI5+CXYYhkA\n" + 
-"0wVj5hMAdNE26SmUNbJY1RsGfNYYOWqRT5If11ECgYEAkx/dZN9+lPqec/mvZ3pD\n" + 
-"/E8jbhGG3G1cg0Er9uAziQFK7yjT3/a1ZqOqyFVaumy/h95nNe8heri2A8fLCF7P\n" + 
-"34oBls+A5w3QFuiQEV6J3tWYXiV0+6dQ7oH7stJrwkVTJ9B27zPlb6SL8zOW4XWr\n" + 
-"bGCgokcPTXoY67gAObx53Vs=\n" + 
+"MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDl8JPChLBfKjHa\n" + 
+"Kqw1rWxQKR/31aXikR+ZCUkVOhP+N9BqMLskizWAnFIIq5iTI0ErYO6D2d+Rrn+S\n" + 
+"YpbNPiNCp1+WkmZwDl3oRHIEL01Qul21eQFFss0HVD6Bed/ABWkQuxRZlo2NFVMS\n" + 
+"9sD0nFzWlGjk6DkFvgEikwgsTKzuF3FusCpajTFm0dR2V7B4OGTdlnOv8fq57pRA\n" + 
+"xJ1kdK5h53trtrve+HrAdAgJj2QdhtJRkg7UvqEroR7NBjgb0T4rkgfPKDvtRl1t\n" + 
+"+sSePu9a41zxFQ7PXSjxcTUPBu+emgLwhCI+f7ijX4O4xd9UFM7m5RDU7Rxzp74j\n" + 
+"lfezw3I/AgMBAAECggEBAMQysuGXNqb86eyt3KMwhusfLBfcRN891ShPs/xYwhZ4\n" + 
+"qWzyh7x2zAAhYh3jzRw/SKwq2VnH3ewAaPoPBX27N3r4NafU43NZzucQ//hyJBZt\n" + 
+"7ueZiGxgVHGcgHkZ9MFz3GJaPtLyk3V+bJQR2DLf+JdfquEnBQDRT0ahDqg+BJBh\n" + 
+"8kCwJ5G4LMoD04x2n4OF9F5iCueVjOVQFEZMiffYiHBRDGLqOeDZNgX94ZnM7Yrt\n" + 
+"Nl8RR0V1VCGM4L4Rx1Csc+x38+E2inwb4A/SvtIIZthd9nNIHkg9X5eayq2BL4a2\n" + 
+"gzRtPbPRG4XYAwlXzbVNm8NPxBO2fgcfJekjoU2LeAECgYEA+cGT1I/MXLmpN55o\n" + 
+"VNGTLs7hM+OrXqcJOnC+zNlpLZ2YixSqCcASE8SfdrRN02jg874dFdKInzsgSBl0\n" + 
+"RVNE8M030tLS9K8ZiWdOECxK4AFx7CkYDuKXIm6xlZbf5oNPKPDCUggPzbNfOr/W\n" + 
+"pdGz3yr4cHAUeBq4fpuVyFb0e/8CgYEA67AtEi0NdFiOElTGgRtzOGGrBnluSg9k\n" + 
+"1LFUCq58OsZjnBZXfwQ5SXf3i5Wlu/V++BVKKsk9b1b4zr2X7hWWUOq2pMrqpk5V\n" + 
+"bMRMrwDAvv5NHX48DwMiSAthfUxL0cTCa1hib3Km7ftpWsPtSbXR4RSTAtKYit5C\n" + 
+"CAuCccrqCcECgYEAxPmVxLPwgkTvH21wbUyoXudMl6b8Vfc5AP1AjcD+AbrkPvR6\n" + 
+"Mpxn5W1SMsV7B7wUhkevGrHjjGmOSS7CE5bbrWq8lyostEuQwVxXJcw49ThOh+nV\n" + 
+"DpBIkCBrMEZAqcVv3iMbrqSrChlohqYb/MVJrj1umQbcLektDrVYSRvDUDMCgYEA\n" + 
+"tDoFTSfcaQKqqYPgQ6v9ALlW8d17o/B/l1F+xahF4SAB3cML51oQgIjXaAroMIH7\n" + 
+"NLP7Ahre+rwUCOvcOTiSuI+zWPK+Wqv+EO1PAmfd/G80AwCb5pLr7RGe3BSyydbf\n" + 
+"IPz2UOjok4U0PC8kzb/WnXqBLKBj+5UYA1ThzChxrUECgYBHNWU+U73eI0t3eshF\n" + 
+"LRG73tlIcSHWVHOIQj7a4Eah+oHfWBAOXz8SrcPyCJOzPQuIn12y7fHMaBuBVdu2\n" + 
+"GVIghp5ztgXYWakpAxR1N1RFx04zFaAiBKFUesQYV8QpN+EkSOFORGnkPBIEJ4GS\n" + 
+"XxwqM7+VsuQCNx2WcHmO4bDN2A==\n" + 
 "-----END PRIVATE KEY-----";
 
 qz.security.setSignatureAlgorithm("SHA512"); // Since 2.1
@@ -72,7 +72,7 @@ qz.security.setSignaturePromise(function(toSign) {
     return function(resolve, reject) {
         try {
             var pk = KEYUTIL.getKey(privateKey);
-            var sig = new KJUR.crypto.Signature({"alg": "SHA512withRSA"});  // Use "SHA1withRSA" for QZ Tray 2.0 and older
+            var sig = new KJUR.crypto.Signature({"alg": "SHA512"});  // Use "SHA1withRSA" for QZ Tray 2.0 and older
             sig.init(pk); 
             sig.updateString(toSign);
             var hex = sig.sign();
